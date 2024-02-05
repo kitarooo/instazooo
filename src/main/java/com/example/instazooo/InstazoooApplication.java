dda2621c5e69d2f473845ffbcbd22329e9d3fccb
kitarooo <bravlstarstop05@mail.ru>
2024-02-05 22:13:43 +0300

    created carcase for spring backend

--- src/main/java/com/example/instazooo/InstazoooApplication.java
+++ src/main/java/com/example/instazooo/InstazoooApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InstazoooApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InstazoooApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InstazoooApplication.class, args);
+    }
 
 }
