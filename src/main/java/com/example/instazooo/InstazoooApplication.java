3e27a39e618b66271d5d32b9dda4162082cf8a25
kitarooo <bravlstarstop05@mail.ru>
2024-02-05 22:11:17 +0300

    Initial commit

---
+++ src/main/java/com/example/instazooo/InstazoooApplication.java
@@ -0,0 +1,13 @@
+package com.example.instazooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstazoooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstazoooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/instazooo/InstazoooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instazooo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstazoooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
